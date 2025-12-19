aaef31b65e7f10b13e87c5861823545a90adf464
alfiyaafroz786-crypto <alfiyaafroz786@gmail.com>
2025-12-19 19:41:48 +0530

    Add files via upload

---
+++ routes/home.js
@@ -0,0 +1,21 @@
+module.exports = (req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.end(`
+    <html>
+      <head>
+        <title>Home</title>
+        <link rel="stylesheet" href="/style.css">
+      </head>
+      <body>
+        <h1>Home Page</h1>
+        <nav>
+          <a href="/home">Home</a>
+          <a href="/about">About</a>
+          <a href="/contact">Contact</a>
+          <a href="/services">Services</a>
+        </nav>
+        <p>Welcome to our website.</p>
+      </body>
+    </html>
+  `);
+};
